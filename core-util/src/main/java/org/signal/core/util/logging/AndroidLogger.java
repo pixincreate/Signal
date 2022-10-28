b435a68509a7e5c84136ac6175b34ea6f4a562ae
Abandoned Cart <twistedumbrella@gmail.com>
2022-10-28 18:32:16 -0400

    Add a flag to ignore excessive use of verbose log

--- core-util/src/main/java/org/signal/core/util/logging/AndroidLogger.java
+++ core-util/src/main/java/org/signal/core/util/logging/AndroidLogger.java
@@ -4,22 +4,28 @@ import android.annotation.SuppressLint;
 
 import org.signal.core.util.logging.Log;
 
+import org.signal.core.util.BuildConfig;
+
 @SuppressLint("LogNotSignal")
 public final class AndroidLogger extends Log.Logger {
 
+  private boolean isLogVerbose() {
+    return BuildConfig.VERBOSE_LOGGING;
+  }
+
   @Override
   public void v(String tag, String message, Throwable t, boolean keepLonger) {
-    android.util.Log.v(tag, message, t);
+    if (isLogVerbose()) android.util.Log.v(tag, message, t);
   }
 
   @Override
   public void d(String tag, String message, Throwable t, boolean keepLonger) {
-    android.util.Log.d(tag, message, t);
+    if (isLogVerbose()) android.util.Log.d(tag, message, t);
   }
 
   @Override
   public void i(String tag, String message, Throwable t, boolean keepLonger) {
-    android.util.Log.i(tag, message, t);
+    if (isLogVerbose()) android.util.Log.i(tag, message, t);
   }
 
   @Override
