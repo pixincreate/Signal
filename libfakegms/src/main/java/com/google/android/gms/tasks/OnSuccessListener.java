tw-hx/Signal-Android

Stub out Google Mobile Services and Firebase with Open Source library, new build flavor 'gms' to use it

libfakegms/src/main/java/com/google/android/gms/tasks/OnSuccessListener.java
@@ -1,0 +1,5 @@
+package com.google.android.gms.tasks;
+
+public interface OnSuccessListener<T> {
+  void onSuccess(T result);
+}

pixincreate/Signal

libfakegms/src/main/java/com/google/android/gms/tasks/OnSuccessListener.java
@@ -1,0 +1,5 @@
+package com.google.android.gms.tasks;
+
+public interface OnSuccessListener<TResult> {
+  void onSuccess(TResult result);
+}